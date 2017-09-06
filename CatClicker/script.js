07e6d6cace93c4aabae6b90960abac38f759c331
JacobWylie <jacobwylie@gmail.com>
2017-09-06 16:11:59 +0200

    cat clicker 2 finished

--- CatClicker/script.js
+++ CatClicker/script.js
@@ -1,10 +1,33 @@
-const cat = document.querySelector('.cat');
-const count = document.querySelector('.count');
+///////////////////////////////////////////////////
+// One cat to click on - No Names
+///////////////////////////////////////////////////
+
+// const cat = document.querySelector('img');
+// const count = document.querySelector('.count');
+// let i = 0;
+
+// function catClicker() {
+// 	i = i + 1;
+// 	count.textContent = i;
+// }
+
+// cat.addEventListener('click', catClicker);
+
+
+///////////////////////////////////////////////////
+// Two cats to click on - Cats have  Names
+///////////////////////////////////////////////////
+
+const cat = document.querySelectorAll('.cat');
 let i = 0;
 
 function catClicker() {
-	i = i + 1;
+	count = this.parentNode.querySelector('.count');
+	i = Number(count.textContent) + 1;
+	i.toString();
 	count.textContent = i;
 }
 
-cat.addEventListener('click', catClicker);
+cat.forEach( cat => {
+	cat.querySelector('img').addEventListener('click', catClicker);
+});
