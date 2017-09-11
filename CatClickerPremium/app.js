f36f8d1115040dbe9be6e1e166b93fb99cdb776d
JacobWylie <jacobwylie@gmail.com>
2017-09-11 16:15:05 +0200

    cat clicker premium setup

---
+++ CatClickerPremium/app.js
@@ -0,0 +1,38 @@
+///////////////////////////
+// Model
+///////////////////////////
+
+const model = {
+	currentCat: null,
+	cats: [
+		{
+			clickCount: 0,
+			name: 'Tabby',
+			imgSrc: 'img/cat1'
+		},
+		{
+			clickCount: 0,
+			name: 'Tiger',
+			imgSrc: 'img/cat2'
+		},
+		{
+			clickCount: 0,
+			name: 'Fluffy',
+			imgSrc: 'img/cat3'
+		},
+		{
+			clickCount: 0,
+			name: 'Hairball',
+			imgSrc: 'img/cat4'
+		},
+		{
+			clickCount: 0,
+			name: 'Scout',
+			imgSrc: 'img/cat5'
+		},
+	]
+}
+
+///////////////////////////
+// Controller
+///////////////////////////
